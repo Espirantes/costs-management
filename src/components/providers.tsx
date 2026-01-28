36126fb0b762a52f09acdf49e8e9c0cde621b2c8
espirantes <dominik@digitalmakers.cz>
2026-01-28 22:30:08 +0100

    Fix SessionProvider basePath for production

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -5,7 +5,7 @@ import { Toaster } from "@/components/ui/sonner";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <SessionProvider>
+    <SessionProvider basePath="/api/auth" refetchOnWindowFocus={true}>
       {children}
       <Toaster />
     </SessionProvider>
